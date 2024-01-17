24522ba975457b57a7ef3de0e3f83246820d97f1
unknown <pvs.abhinav@gmail.com>
2024-01-16 18:06:49 -0800

    fixed zero heading button binding

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,8 +8,7 @@ import frc.robot.subsystems.SwerveModule;
 import frc.robot.subsystems.SwerveSubsystem;
 import frc.robot.Constants.OIConstants;
 import frc.robot.commands.SwerveJoystickCmd;
-
-
+import frc.robot.commands.ZeroHeadingCmd;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -25,6 +24,9 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   public static SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
+
+  //commands
+  ZeroHeadingCmd zeroheading = new ZeroHeadingCmd(swerveSubsystem);
 
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
@@ -55,7 +57,7 @@ public class RobotContainer {
    */
   private void configureBindings() {
     // joystick 1
-    //new JoystickButton(controller1, 6).whenPressed(() -> swerveSubsystem.zeroHeading());
+    new JoystickButton(controller1, 6).onTrue(zeroheading);
 
     // joystick 2
     // new JoystickButton(joystick2, 10).whenPressed(() -> swerveSubsystem.dReset()); // remove this after ONLY FOR AUTO TESTING!!!!
